40d84027e65121d6186813290cc89bb25330595c
Shubham Naidu <shubham.naidu@webengage.com>
2025-05-02 08:16:22 +0530

    Initial commit

--- Package.swift
+++ Package.swift
@@ -14,8 +14,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "WebEngageSPM"),
+        
+        .binaryTarget(
+            name: "WebEngageSPM",
+            url: "https://s3-us-west-2.amazonaws.com/webengage-sdk/ios/WebEngageCore/6.15.0/WebEngageFramework.zip", checksum: "6cbf358c9e875f0e5c9eb3c37c57a725b307e285018c4f7dd45fcdf5b635f739"
+        ),
         .testTarget(
             name: "WebEngageSPMTests",
             dependencies: ["WebEngageSPM"]
