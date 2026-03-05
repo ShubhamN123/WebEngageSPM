dadeb3473c2fcaad9535985b91df6dcb700ab646
Shubham Naidu <shubham.naidu@webengage.com>
2026-03-05 08:45:46 +0530

    removed local frameworks and used links

--- Package.swift
+++ Package.swift
@@ -10,9 +10,25 @@ let package = Package(
         .library(name: "WEPersonalization", targets: ["WEPersonalization"])
     ],
     targets: [
-        .binaryTarget(name: "WebEngage", path: "Sources/WebEngageSPM/xcFrameworks/WebEngage.xcframework"),
-        .binaryTarget(name: "WELocation", path: "Sources/WebEngageSPM/xcFrameworks/WELocation.xcframework"),
-        .binaryTarget(name: "WENotificationInbox", path: "Sources/WebEngageSPM/xcFrameworks/WENotificationInbox.xcframework"),
-        .binaryTarget(name: "WEPersonalization", path: "Sources/WebEngageSPM/xcFrameworks/WEPersonalization.xcframework")
+        .binaryTarget(
+            name: "WebEngage",
+            url: "https://tmpfiles.org/dl/27354503/webengage.xcframework.zip",
+            checksum: "b94c92e9d3b47b4c2f1bb93b24e25809fcd8a3d80e54006c2c4dce0b7a7339f5"
+        ),
+        .binaryTarget(
+            name: "WELocation",
+            url: "https://tmpfiles.org/dl/27354704/welocation.xcframework.zip",
+            checksum: "5314d41eb0ecfa553c94b1d42d9ed26cd84ca1114c20613e57edbe96a7589f68"
+        ),
+        .binaryTarget(
+            name: "WENotificationInbox",
+            url: "https://tmpfiles.org/dl/27354748/wenotificationinbox.xcframework.zip",
+            checksum: "d9928f2960ddb7422e509dfd3e33b0235bec3fa7537486878aef1b2ffc567b83"
+        ),
+        .binaryTarget(
+            name: "WEPersonalization",
+            url: "https://tmpfiles.org/dl/27354769/wepersonalization.xcframework.zip",
+            checksum: "1bf628c7b7b2502e6956a817deca84a544ec946841e1ca795b4072ced14b2216"
+        )
     ]
 )
